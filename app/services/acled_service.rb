2c6a46795a6a0168d0e0e67d8d6be493cc4eb9ee
scott <scott.m.burdick@gmail.com>
2019-07-28 09:51:48 -0400

    Fix acled_service to return data portion only of response

--- app/services/acled_service.rb
+++ app/services/acled_service.rb
@@ -2,7 +2,7 @@ class AcledService
 
   def get_conflict_by_country(country)
     response = HTTP.get("https://api.acleddata.com/acled/read?terms=accept&country=#{country}").to_s
-    parsed_response = JSON.parse(response)
+    parsed_response = JSON.parse(response)["data"]
   end
-  
+
 end
