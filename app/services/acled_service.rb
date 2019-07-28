6ff8922dc573500444a967700bc2274228c36aed
scott <scott.m.burdick@gmail.com>
2019-07-28 18:00:03 -0400

    Change limit for service query

--- app/services/acled_service.rb
+++ app/services/acled_service.rb
@@ -1,7 +1,7 @@
 class AcledService
 
   def get_conflict_by_country(country)
-    response = HTTP.get("https://api.acleddata.com/acled/read?terms=accept&country=#{country}").to_s
+    response = HTTP.get("https://api.acleddata.com/acled/read?terms=accept&country=#{country}&limit=1000").to_s
     parsed_response = JSON.parse(response)["data"]
   end
 
