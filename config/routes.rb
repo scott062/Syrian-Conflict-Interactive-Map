72ef5ba981bf5f82de69a2a67aff9e218670dc14
scott <scott.m.burdick@gmail.com>
2019-07-28 09:31:01 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #format json option directs controller to jbuilder files instead of html.erb
+  namespace :api, defaults: { format: :json } do
+    resources :conflicts, only: [:index, :show]
+  end
 end
