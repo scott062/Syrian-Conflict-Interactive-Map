3206588385758242876473c6e5d8c4765cdf7d01
scott <scott.m.burdick@gmail.com>
2019-07-28 09:39:22 -0400

    Add acled service class and request method

---
+++ app/services/acled_service.rb
@@ -0,0 +1,8 @@
+class AcledService
+
+  def get_conflict_by_country(country)
+    response = HTTP.get("https://api.acleddata.com/acled/read?terms=accept&country=#{country}").to_s
+    parsed_response = JSON.parse(response)
+  end
+  
+end
