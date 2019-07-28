53a55d5ab1873c5df301f6dde0cc97b2c3b01d0f
scott <scott.m.burdick@gmail.com>
2019-07-28 11:00:56 -0400

    Add static pages to hook into react elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'static_pages#root'
   #format json option directs controller to jbuilder files instead of html.erb
   namespace :api, defaults: { format: :json } do
     resources :conflicts, only: [:index, :show]
